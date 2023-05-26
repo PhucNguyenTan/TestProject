ffe36cd1e1708fdb2d5cd76bb6497bd20e30bf98
Phuced <neiT480@gmail.com>
2023-05-27 05:49:22 +0700

    Checking

--- Assets/Scripts/UIScroll/ScrollBar/ScrollBarMinimum.cs
+++ Assets/Scripts/UIScroll/ScrollBar/ScrollBarMinimum.cs
@@ -25,10 +25,12 @@ namespace UIScroll.Scroll_bar
 
         private void SetCustomScrollbar(float scrollValue)
         {
-            var minY = scrollValue * (sizeMax - sizeMin);
-            var maxY = handleCustom.anchorMin.y + sizeMin;
+            var minY = Mathf.Min(sizeMax, scrollValue * (sizeMax - sizeMin));
+            //var maxY = handleCustom.anchorMin.y + sizeMin;
+            //var maxY = Mathf.Min(sizeMax, minY + Mathf.Max(0f, sizeMin - Mathf.Abs(scrollValue - 0.5f) * 2f));
+            var maxY = Mathf.Max(0f, minY + Mathf.Max(0f, sizeMin - Mathf.Abs(scrollValue - 0.5f) * 2f));
             handleCustom.anchorMin = new Vector2(handleCustom.anchorMin.x, minY);
-            handleCustom.anchorMax = new Vector2(handleCustom.anchorMin.x, maxY);
+            handleCustom.anchorMax = new Vector2(handleCustom.anchorMax.x, maxY);
         }
     }
 
